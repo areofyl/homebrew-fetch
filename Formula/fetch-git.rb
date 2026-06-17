fb2300d9df1767a3173742bc651c6026b7bc1b56
areofyl <areofyl@users.noreply.github.com>
2026-06-16 18:04:23 -0700

    add fetch-git formula

---
+++ Formula/fetch-git.rb
@@ -0,0 +1,19 @@
+class FetchGit < Formula
+  desc "3D rotating system fetch for your terminal"
+  homepage "https://github.com/areofyl/fetch"
+  url "https://github.com/areofyl/fetch/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "575f43040fd3f912f84a151ee4069812eb1a15306541ed36d5b10897984174ed"
+  license "ISC"
+
+  head "https://github.com/areofyl/fetch.git", branch: "master"
+
+  depends_on "fastfetch"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_predicate bin/"fetch", :exist?
+  end
+end
