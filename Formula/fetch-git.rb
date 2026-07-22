60661cc9704e7de07d3deb901e42cd2dc7973fc7
areofyl <areofyl@users.noreply.github.com>
2026-07-21 18:21:15 -0700

    bump to v2.2.0 with macos support

--- Formula/fetch-git.rb
+++ Formula/fetch-git.rb
@@ -1,8 +1,8 @@
 class FetchGit < Formula
   desc "3D rotating system fetch for your terminal"
   homepage "https://github.com/areofyl/fetch"
-  url "https://github.com/areofyl/fetch/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "575f43040fd3f912f84a151ee4069812eb1a15306541ed36d5b10897984174ed"
+  url "https://github.com/areofyl/fetch/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "a09f78b5bd6007b7260bfa7cf2879944ba48ef6bdefbd98c3841e00255daae2c"
   license "ISC"
 
   head "https://github.com/areofyl/fetch.git", branch: "main"
